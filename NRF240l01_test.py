cbd4c00986de90cde34f3cc3e2f88aa6330d389f
vendetta <vendetta1987@freenet.de>
2023-02-02 21:21:30 +0100

    added pi pico temperature reading; fixed double package error by adding wait for send done manually send() doesn't work properly

--- NRF240l01_test.py
+++ NRF240l01_test.py
@@ -1,6 +1,8 @@
+import random
+
 import ustruct as struct
 import utime
-from machine import SPI, Pin
+from machine import ADC, SPI, Pin
 
 from NRF24L01 import NRF24L01, POWER_3, SPEED_250K
 from Weatherstation import WeatherStation
@@ -26,18 +28,44 @@ class NRFManager:
         self.nrf.open_rx_pipe(1, self.addresses[1])
 
 
+def GetPicoTemperature():
+    reading = sensor_temp.read_u16() * conversion_factor
+    return 27 - (reading - 0.706)/0.001721
+
+
+sensor_temp = ADC(4)
+conversion_factor = 3.3 / (65535)
+
 if __name__ == "__main__":
-    mngr = NRFManager()
+    nrf_mngr = NRFManager()
     ws = WeatherStation()
 
+    cnt = 0
+    wait_cnt = 0
     while True:
-        millis = utime.ticks_ms()
-        try:
-            bin = ws.Serialize()
-            mngr.nrf.send(bin)
-        except OSError:
-            pass
-        else:
-            print("sending "+str(bin))
+        if (cnt > 0) and (nrf_mngr.nrf.send_done() is None) and (wait_cnt < 100):
+            wait_cnt = wait_cnt+1
+            utime.sleep_us(10)
+            continue
 
-        utime.sleep_ms(100)
+        wait_cnt = 0
+        ticks_begin = utime.ticks_ms()
+
+        try:
+            ws.Temperature = GetPicoTemperature()
+            ws.Humidity = random.randrange(0, 1000)/10
+
+            bin = ws.Serialize()
+            bin = struct.pack("i", cnt)+bin
+
+            print(
+                f"sending {cnt}: t={ws.Temperature}, h={ws.Humidity} -> {str(bin)}")
+            nrf_mngr.nrf.send_start(bin)
+        except:
+            print(f"error at {cnt}")
+
+        cnt = cnt+1
+
+        duration = utime.ticks_diff(utime.ticks_ms(), ticks_begin)
+        print(duration)
+        utime.sleep_ms(1000-duration)
