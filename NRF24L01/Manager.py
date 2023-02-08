3305ccfc8bfa09f6facbadb8c25d7a88bb42af57
vendetta <vendetta1987@freenet.de>
2023-02-08 20:19:34 +0100

    moved files, renamed and used proper package imports

---
+++ NRF24L01/Manager.py
@@ -0,0 +1,38 @@
+import utime
+from machine import SPI, Pin
+
+from NRF24L01.Driver import NRF24L01, POWER_3, SPEED_250K
+
+
+class NRFManager:
+    # letter -> ASCII -> hex
+    # hex(ord("W"))
+    # hex(ord("w"))
+    addresses = (b"\x57\x57\x57\x57\x57", b"\x77\x77\x77\x77\x77")
+
+    def __init__(self):
+        self.spi = SPI(0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
+        self.cfg = {"spi": self.spi, "csn": 0, "ce": 6}
+
+        self.csn = Pin(self.cfg["csn"], mode=Pin.OUT)
+        self.ce = Pin(self.cfg["ce"], mode=Pin.OUT)
+
+        self.nrf = NRF24L01(self.cfg["spi"], self.csn,
+                            self.ce, channel=100, payload_size=32)
+        self.nrf.set_power_speed(POWER_3, SPEED_250K)
+        print(self.spi)
+
+        self.nrf.open_tx_pipe(self.addresses[0])
+        self.nrf.open_rx_pipe(1, self.addresses[1])
+
+    def send(self, data: bytes):
+        try:
+            self.nrf.send_start(data)
+        except:
+            print(f"error sending {data}")
+            return
+
+        wait_cnt = 0
+        while (self.nrf.send_done() is None) and (wait_cnt < 100):
+            wait_cnt = wait_cnt+1
+            utime.sleep_us(10)
--- NRF24L01/NRF240l01_test.py
+++ NRF24L01/NRF240l01_test.py
@@ -2,30 +2,10 @@ import random
 
 import ustruct as struct
 import utime
-from machine import ADC, SPI, Pin
+from machine import ADC
 
-from NRF24L01 import NRF24L01, POWER_3, SPEED_250K
+from NRF24L01.Manager import NRFManager
 from Weatherstation import WeatherStation
-
-
-class NRFManager:
-    # 57 == ASCII W; 77 == ASCII w
-    addresses = (b"\x57\x57\x57\x57\x57", b"\x77\x77\x77\x77\x77")
-
-    def __init__(self):
-        self.spi = SPI(0, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
-        self.cfg = {"spi": self.spi, "csn": 0, "ce": 6}
-
-        self.csn = Pin(self.cfg["csn"], mode=Pin.OUT)
-        self.ce = Pin(self.cfg["ce"], mode=Pin.OUT)
-
-        self.nrf = NRF24L01(self.cfg["spi"], self.csn,
-                            self.ce, channel=100, payload_size=32)
-        self.nrf.set_power_speed(POWER_3, SPEED_250K)
-        print(self.spi)
-
-        self.nrf.open_tx_pipe(self.addresses[0])
-        self.nrf.open_rx_pipe(1, self.addresses[1])
 
 
 def GetPicoTemperature():
@@ -39,18 +19,9 @@ conversion_factor = 3.3 / (65535)
 if __name__ == "__main__":
     nrf_mngr = NRFManager()
     ws = WeatherStation()
-
     cnt = 0
-    wait_cnt = 0
+
     while True:
-        if (cnt > 0) and (nrf_mngr.nrf.send_done() is None) and (wait_cnt < 100):
-            wait_cnt = wait_cnt+1
-            utime.sleep_us(10)
-            continue
-
-        wait_cnt = 0
-        ticks_begin = utime.ticks_ms()
-
         try:
             ws.Temperature = GetPicoTemperature()
             ws.Humidity = random.randrange(0, 1000)/10
@@ -60,12 +31,9 @@ if __name__ == "__main__":
 
             print(
                 f"sending {cnt}: t={ws.Temperature}, h={ws.Humidity} -> {str(bin)}")
-            nrf_mngr.nrf.send_start(bin)
+            nrf_mngr.send(bin)
         except:
             print(f"error at {cnt}")
 
         cnt = cnt+1
-
-        duration = utime.ticks_diff(utime.ticks_ms(), ticks_begin)
-        print(duration)
-        utime.sleep_ms(1000-duration)
+        utime.sleep_ms(1000)
