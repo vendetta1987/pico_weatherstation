f12493101541c6d88799d840ab5111a7e8446976
vendetta <vendetta1987@freenet.de>
2023-02-15 22:30:46 +0100

    removed time and added ticks to measure time between samples

--- DS15901/TriggerCounter.py
+++ DS15901/TriggerCounter.py
@@ -1,25 +1,30 @@
 from machine import Pin
+import time
 
 
 class TriggerCounter:
     _trigger_count: int = 0
-    measurement_time_sec: int
     _per_trigger_multi: float
     _pin: Pin
+    _start_measure_time: int
 
-    def __init__(self, measurement_time_sec: int, per_trigger_multi: float, pin_number: int):
-        self.measurement_time_sec = measurement_time_sec
+    def __init__(self, per_trigger_multi: float, pin_number: int):
         self._per_trigger_multi = per_trigger_multi
 
         self._pin = Pin(pin_number, Pin.IN, Pin.PULL_DOWN)
         self._pin.irq(handler=self._callback, trigger=Pin.IRQ_RISING)
+        self._start_measure_time = time.ticks_ms()
 
     def ReadAndResetValue(self):
-        tmp = self._trigger_count
+        tmp_count = self._trigger_count
         self._trigger_count = 0
 
-        tmp = tmp/self.measurement_time_sec*self._per_trigger_multi
-        return tmp
+        time_diff_sec = time.ticks_diff(
+            time.ticks_ms(), self._start_measure_time)/1000
+        self._start_measure_time = time.ticks_ms()
+
+        calculated_value = tmp_count/time_diff_sec*self._per_trigger_multi
+        return calculated_value
 
     def _callback(self, pin: Pin):
         self._trigger_count = self._trigger_count+1
