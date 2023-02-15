2856a5b865f7330681019707dfc0611ce034d43a
vendetta <vendetta1987@freenet.de>
2023-02-15 22:31:23 +0100

    added rain_gauge and initial tests for wind vane

--- Test.py
+++ Test.py
@@ -1,8 +1,10 @@
 import utime
 
+from DS15901.TriggerCounter import TriggerCounter
 from NRF24L01.Manager import NRFManager
 from Weatherstation import WeatherStation
-from DS15901.TriggerCounter import TriggerCounter
+
+import machine
 
 
 def SendSensorReadingsByNRF():
@@ -16,14 +18,30 @@ def SendSensorReadingsByNRF():
         utime.sleep_ms(250)
 
 
-def SetupAndWaitForInterrupt():
-    tc = TriggerCounter(3, 2.4, 15)
+def ReadTriggers():
+    anemometer = TriggerCounter(2.4, 15)
+    rain_gauge = TriggerCounter(0.2794, 14)
     while True:
-        utime.sleep(tc.measurement_time_sec)
-        speed_kmh = tc.ReadAndResetValue()
-        print(f"wind speed={speed_kmh}")
+        utime.sleep(3)
+        speed_kmh = anemometer.ReadAndResetValue()
+        rain_mm = rain_gauge.ReadAndResetValue()
+        print(f"wind speed={speed_kmh} mm of rain={rain_mm}")
+
+
+def ReadADC():
+    adc = machine.ADC(26)
+    min = 66000
+    max = 0
+    while True:
+        adc_value = adc.read_u16()
+        if min > adc_value:
+            min = adc_value
+        if max < adc_value:
+            max = adc_value
+        print(f"min={min} max={max} adc value={adc_value}")
 
 
 if __name__ == "__main__":
     # SendSensorReadingsByNRF()
-    SetupAndWaitForInterrupt()
+    # ReadTriggers()
+    ReadADC()
