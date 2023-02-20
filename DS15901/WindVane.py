b7757375efa432d0b687fd9b2adea6a8dc60d91f
vendetta <vendetta1987@freenet.de>
2023-02-20 21:47:32 +0100

    added actual voltage divider circuit; moved code to class file; added table for calculations

--- DS15901/WindVane.py
+++ DS15901/WindVane.py
@@ -1,8 +1,63 @@
+import uasyncio
 from machine import ADC
+
+direction_lut = {
+    1: "E",
+    4: "SE",
+    6: "S",
+    10: "NE",
+    12: "W",
+    14: "N",
+    15: "NW"
+}
 
 
 class WindVane:
     _adc: ADC
+    _histogram: list
+    _continue_measure: bool
+    _direction_count: int = 16
 
     def __init__(self, pin: int):
         self._adc = ADC(pin)
+        self._histogram = [0]*self._direction_count
+
+    def StartMeasurements(self):
+        self._continue_measure = True
+
+        uasyncio.create_task(self.Evaluate())
+        uasyncio.run(self.Measure())
+
+    async def Measure(self):
+        min_value = 65536
+        max_value = 0
+
+        while self._continue_measure:
+            adc_value = self._adc.read_u16()
+
+            min_value = min(min_value, adc_value)
+            max_value = max(max_value, adc_value)
+
+            # print(f"min={min_value}\tmax={max_value}\tadc={adc_value}")
+
+            if max_value > min_value:
+                diff = max_value-min_value
+                window = (adc_value-min_value)/diff
+                idx = round(window*self._direction_count)
+
+                if 0 <= idx < self._direction_count:
+                    self._histogram[idx] += 1
+
+            await uasyncio.sleep_ms(100)
+
+    async def Evaluate(self):
+        while True:
+            await uasyncio.sleep(2)
+            max_value = max(self._histogram)
+
+            if max_value > 0:
+                for i in range(self._direction_count):
+                    bar = "#"*int(self._histogram[i]/max_value*25)
+                    print(f"{i}\t"+bar)
+
+            self._histogram = [0]*self._direction_count
--- Test.py
+++ Test.py
@@ -1,11 +1,9 @@
 import utime
 
 from DS15901.TriggerCounter import TriggerCounter
+from DS15901.WindVane import WindVane
 from NRF24L01.Manager import NRFManager
 from Weatherstation import WeatherStation
-
-import machine
-from math import sqrt
 
 
 def SendSensorReadingsByNRF():
@@ -30,75 +28,8 @@ def ReadTriggers():
 
 
 def ReadADC():
-    adc = machine.ADC(26)
-
-    min_value = 65536
-    max_value = 0
-    histogram_size = 100
-    histogram = [0]*histogram_size
-    loop_cnt = 0
-
-    while True:
-        if (loop_cnt > 0) and ((loop_cnt % 10000) == 0):
-            for i in range(len(histogram)-1):
-                bar = "#"*int(histogram[i]/10)
-                print(f"{i}\t"+bar)
-
-            avg = 0
-            avg_sample_cnt = 0
-            for i in range(len(histogram)-1):
-                if histogram[i] > 0:
-                    avg += histogram[i]
-                    avg_sample_cnt += 1
-
-            if avg_sample_cnt > 0:
-                avg /= avg_sample_cnt
-
-            avg_val_idx = 0
-            variance = 0
-
-            if avg > 0:
-                variance_sample_cnt = 0
-
-                min_diff = 4096
-
-                for i in range(len(histogram)-1):
-                    if histogram[i] > 0:
-                        tmp_diff = histogram[i]-avg
-                        variance += pow(tmp_diff, 2)
-                        variance_sample_cnt += 1
-
-                        if 0 <= tmp_diff < min_diff:
-                            min_diff = tmp_diff
-                            avg_val_idx = i
-
-                if variance_sample_cnt > 1:
-                    variance /= variance_sample_cnt-1
-                    variance = sqrt(variance)
-
-                avg_val_idx = 4095-(histogram_size-avg_val_idx)
-
-            print(
-                f"adc_val={avg_val_idx} avg={avg} variance={variance} hist_size={histogram_size} min={min_value} max={max_value}")
-
-            if max_value > min_value:
-                histogram_size = max_value-min_value
-
-            histogram = [0]*histogram_size
-            loop_cnt = 0
-
-        adc_value = (int)(adc.read_u16()/65535*4095)
-
-        min_value = min(min_value, adc_value)
-        max_value = max(max_value, adc_value)
-
-        idx = adc_value-(4096-histogram_size)
-
-        if 0 <= idx < len(histogram):
-            histogram[idx] += 1
-
-        loop_cnt += 1
-        utime.sleep_us(10)
+    wv = WindVane(26)
+    wv.StartMeasurements()
 
 
 if __name__ == "__main__":
