ef4592ffe5b1d52b54ab69c4cdd7a8dc636b740d
vendetta <vendetta1987@freenet.de>
2023-04-24 20:36:02 +0200

    initial commit of receiving part for RPI4; including NRF reciver example integrated with WeatherStation code as well as simple MQTT client

---
+++ RPI_Receiver/Hub.py
@@ -0,0 +1,92 @@
+import argparse
+import json
+import sys
+import time
+import traceback
+
+import pigpio
+from nrf24 import *
+
+from MQTTClient import MQTTCLient
+from Weatherstation import WeatherStation
+
+if __name__ == "__main__":
+    print("Python NRF24 Simple Receiver Example.")
+    # Parse command line argument.
+    arg_parser = argparse.ArgumentParser(
+        prog="simple-receiver.py", description="Simple NRF24 Receiver Example.")
+    arg_parser.add_argument('-n', '--hostname', type=str, default='localhost',
+                            help="Hostname for the Raspberry running the pigpio daemon.")
+    arg_parser.add_argument('-p', '--port', type=int, default=8888,
+                            help="Port number of the pigpio daemon.")
+    arg_parser.add_argument('address', type=str, nargs='?', default='WWWWW',
+                            help="Address to listen to (3 to 5 ASCII characters)")
+    arg_parser.add_argument('ce', type=str, nargs='?', default=23,
+                            help="chip enbale pin")
+    arg_parser.add_argument('channel', type=str, nargs='?', default=100,
+                            help="NRF channel")
+
+    args = arg_parser.parse_args()
+    hostname = args.hostname
+    port = args.port
+    nrf_address = args.address
+    ce_pin = args.ce
+    nrf_channel = args.channel
+
+    client = MQTTCLient()
+    client.Connect()
+
+    # Verify that address is between 3 and 5 characters.
+    if not (2 < len(nrf_address) < 6):
+        print(
+            f'Invalid address {nrf_address}. Addresses must be between 3 and 5 ASCII characters.')
+        sys.exit(1)
+
+    print(f'Connecting to GPIO daemon on {hostname}:{port} ...')
+    pi_gpio_d = pigpio.pi(hostname, port)
+    if not pi_gpio_d.connected:
+        print("Not connected to Raspberry Pi ... goodbye.")
+        sys.exit()
+
+    nrf = NRF24(pi_gpio_d, ce=ce_pin, payload_size=RF24_PAYLOAD.MAX, channel=nrf_channel,
+                data_rate=RF24_DATA_RATE.RATE_250KBPS, pa_level=RF24_PA.MAX, address_bytes=len(nrf_address))
+    nrf.open_reading_pipe(RF24_RX_ADDR.P1, nrf_address)
+    nrf.show_registers()
+
+    ws = WeatherStation()
+
+    try:
+        print(f'Receiving from {nrf_address} on channel {nrf_channel}')
+
+        while True:
+            while nrf.data_ready():
+                payload = nrf.get_payload()
+                print(payload)
+                ws.Deserialize(payload)
+                print(f"T={ws.Temperature:.2f}\tH={ws.Humidity:.2f}\t\
+                      P={ws.Pressure:.2f}\tSM={ws.SoilMoisture:.2f}\t\
+                      ST={ws.SoilTemperature:.2f}\tWD={ws.WindDirection}\t\
+                      WS={ws.WindSpeed:.2f}\tR={ws.Rain:.2f}")
+
+                client.Publish("temperature", ws.Temperature)
+                client.Publish("humidity", ws.Humidity)
+                client.Publish("pressure", ws.Pressure)
+                client.Publish("rain", ws.Rain)
+
+                soil = {
+                    "moisture": ws.SoilMoisture,
+                    "temperature": ws.SoilTemperature
+                }
+                client.Publish("soil", json.dumps(soil))
+
+                wind = {
+                    "direction": ws.WindDirection,
+                    "speed": ws.WindSpeed,
+                }
+                client.Publish("wind", json.dumps(wind))
+
+            time.sleep(0.001)
+    except:
+        traceback.print_exc()
+        nrf.power_down()
+        pi_gpio_d.stop()
---
+++ RPI_Receiver/MQTTClient.py
@@ -0,0 +1,51 @@
+import time
+
+import paho.mqtt.client as mqtt
+
+
+class MQTTCLient:
+    _TOPIC: str = "Weatherstation"
+    _client: mqtt.Client
+    _address: str
+    _port: int
+    is_connected: bool
+
+    def __init__(self, address: str = "localhost", port: int = 1883) -> None:
+        self.is_connected = False
+
+        self._address = address
+        self._port = port
+
+        self._client = mqtt.Client(client_id="weatherstation")
+        self._client.on_connect = self._OnConnect
+        self._client.on_message = self._OnMessage
+
+    def __del__(self):
+        self._client.loop_stop()
+
+    def Connect(self):
+        self._client.connect(self._address, self._port)
+        self._client.loop_start()
+
+        wait_cnt = 0
+        while not self.is_connected:
+            time.sleep(0.01)
+            wait_cnt += 1
+
+            if wait_cnt >= 10:
+                print("problem connecting to MQTT")
+                break
+
+        if self.is_connected:
+            self._client.publish(f"{self._TOPIC}/status", "connected")
+
+    def Publish(self, topic: str, msg: str):
+        if self.is_connected:
+            self._client.publish(f"{self._TOPIC}/{topic}", msg)
+
+    def _OnConnect(self, client, userdata, flags, rc):
+        print("connected to MQTT broker")
+        self.is_connected = rc == 0
+
+    def _OnMessage(self, client, userdata, msg):
+        print(msg.topic+" "+str(msg.payload))
